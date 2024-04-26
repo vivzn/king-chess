3b1350634d25448a1b6ddee3d68eacb5cd3143ad
vivzn <vivaanrum2@gmail.com>
2024-04-26 20:51:26 +0800

    jiejd

--- pusherConfig.ts
+++ pusherConfig.ts
@@ -2,21 +2,15 @@ import PusherServer from 'pusher';
 import PusherClient from 'pusher-js';
 
 export const pusherServer = new PusherServer({
-    appId: "1781690",
-    key: "15619571febcdb15535c",
-    secret: "f082646bd8a93b372a10",
+    appId: "1794195",
+    key: "403e59777051aeefcdad",
+    secret: "d766ae1e53a7b8301b39",
     cluster: "ap1",
-    useTLS: true,
+    useTLS: true
 })
 
-export const pusherClient = new PusherClient("15619571febcdb15535c", {
+export const pusherClient = new PusherClient("403e59777051aeefcdad", {
+
     cluster: "ap1",
-    authEndpoint: "/api/pusher-auth",
-    authTransport: "ajax",
-    auth: {
-        headers: {
-            'Content-Type': "application/json"
-        }
-    }
 });
 
